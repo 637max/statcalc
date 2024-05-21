b6643ca1cbf74d10e6176802c94465edbef8346d
max <90658961+637max@users.noreply.github.com>
2024-05-21 14:19:37 -0400

    Add files via upload

---
+++ statcalc-main/index.js
@@ -0,0 +1,16 @@
+let hNull;
+let Mean;
+let Sx;
+let n;
+let Ha;
+
+document.getElementById("mySubmit").onclick = function(){
+	
+	hNull = document.getElementById("input1").value;
+	Mean = document.getElementById("input1").value;
+	Sx = document.getElementById("input1").value;
+	n = document.getElementById("input1").value;
+	Ha = document.getElementById("input1").value;
+	
+	document.getElementById("hNul").textContent = 'hi';
+}
